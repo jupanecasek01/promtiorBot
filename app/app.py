b076b168c8811bcf017b53a6468063999c844494
unknown <jupanecasek@gmail.com>
2024-11-14 15:55:22 -0300

    Archivos

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from bot import execute_agent  # Asegúrate de que este archivo y función existan
+from bot import execute_agent
+import uvicorn
 
 app = FastAPI()
 
@@ -9,19 +10,29 @@ app = FastAPI()
 async def read_root():
     return {"message": "Welcome to the Promtior Bot API!"}
 
-# Definir el esquema de entrada para la pregunta y el ID de la conversación
+
 class QuestionRequest(BaseModel):
     question: str
     conversation_id: str = ""
 
-# Ruta para recibir preguntas
+
 @app.post("/ask")
 async def ask(data: QuestionRequest):
     question = data.question
     conversation_id = data.conversation_id
+  
+    try:
+        response = execute_agent(question, conversation_id)
+   
+        if "final_response" not in response:
+            raise ValueError("La respuesta no contiene 'final_response'")
+        
+        return {"response": response["final_response"]}
     
-    # Llama a la función de tu bot para obtener la respuesta
-    response = execute_agent(question, conversation_id)
-    
-    # Asegúrate de devolver la respuesta que tiene la clave 'final_response'
-    return {"response": response["final_response"]}
+    except Exception as e:
+       
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
